a18fe997162ddf9abd1ac3b68a5dbf0d5e500b69
Christofer Jungberg <christofer@kodamera.se>
2021-08-16 16:34:48 +0200

    used nuxtlink to get correct url

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,13 +3,13 @@
     <div class="grid gap-6 mb-8 md:grid-cols-1 lg:grid-cols-2">
       <!-- Card 1 -->
       <div v-for="project in projects" :key="project.id" class="flex p-4 bg-white border-2 border-gray-200 rounded-lg shadow-sm dark:bg-gray-800">
-        <a :href="'/projects/' + project.fields.slug">
+        <NuxtLink :to="'/projects/' + project.fields.slug">
           <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" :src="project.fields.image.fields.file.url" />
           <div id="body" class="flex flex-col ml-5 pt-5">
               <h4 id="name" class="text-xl font-semibold mb-2">{{ project.fields.title }}</h4>
               <p id="job" class="text-gray-800 mt-2">{{ project.fields.intro }}</p>
           </div>
-        </a>
+        </NuxtLink>
       </div>
     </div>
   </section>
